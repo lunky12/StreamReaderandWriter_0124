164711209c3c8c916cab4edb82958a1aa13dbd5d
lunky12 <lunkyumy@anda.com>
2026-06-02 18:03:10 +0700

    menunjuk ke sebuah file dengan ekstensi .txt

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -38,3 +38,9 @@ int main()
 
     //membuka file dalam mode membaca
     ifstream infile;
+
+    //menunjuk ke sebuah file dengan ekstensi .txt
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
+    
